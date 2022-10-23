0a887523b32afa3b6570c333bdf47357149f9ebb
pcbennett108 <pcbennett42@gmail.com>
2022-10-23 12:54:58 -0600

    Assign fill color to tile-rect based on data-category

--- script.js
+++ script.js
@@ -19,12 +19,33 @@ let drawTreeMap = () => {
 
   let createTreeMap = d3.treemap().size([1000, 600]);
   let movieTiles = hierarchy.leaves();
+  console.log(movieTiles);
 
   createTreeMap(hierarchy);
 
   let block = canvas.selectAll("g").data(movieTiles).enter().append("g");
 
-  block.append("rect").attr("class", "tile");
+  block
+    .append("rect")
+    .attr("class", "tile")
+    .attr("fill", (movie) => {
+      let category = movie["data"]["category"];
+      if (category === "Action") {
+        return "#845EC2";
+      } else if (category === "Drama") {
+        return "#D65DB1";
+      } else if (category === "Biography") {
+        return "#FF6F91";
+      } else if (category === "Adventure") {
+        return "#FF9671";
+      } else if (category === "Animation") {
+        return "#FFC75F";
+      } else if (category === "Comedy") {
+        return "#F9F871";
+      } else if (category === "Family") {
+        return "#00C9A7";
+      }
+    });
 };
 
 d3.json(movieDataUrl).then((data, error) => {
